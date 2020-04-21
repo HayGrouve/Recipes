0c58aeb232c05db7769df6a0def9c222aa50741d
HayGrouve <haygrouve@gmail.com>
2020-04-21 18:02:06 +0300

    navbar changes

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -11,5 +11,18 @@ $(".fa-plus").click(function () {
         $('#form').fadeToggle(300);
 });
 
+//Toggler
+var open = false;
+$('#toggler').click(function () {
+        if (open) {
+                open = false;
+                $('#toggler-icon').removeClass("fa-chevron-up").addClass("fa-chevron-down");
+        } else {
+                open = true;
+                $('#toggler-icon').removeClass("fa-chevron-down").addClass("fa-chevron-up");
+        }
+
+});
+
 
 
